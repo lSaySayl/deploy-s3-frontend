810353774bed84d8bfa2eeac8fb15cebcd82197d
camilo.ramirez3 <camilo.ramirez@pragma.com.co>
2025-03-26 00:34:05 -0500

    actualizando footer

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -1,7 +1,7 @@
 import "../../styles/Footer.css";
 
 function Footer() {
-    return <footer className="footer">© 2025 - AWS - Devops</footer>;
+    return <footer className="footer">© 2025 - AWS - Azure</footer>;
 }
 
 export default Footer;
